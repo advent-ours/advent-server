3156ef609b97ab39d9c4a3f1bf2a6ba92489588b
JinuCheon <woojin8787@gmail.com>
2023-09-18 23:04:42 +0900

    init

---
+++ src/test/java/com/adventours/calendar/CalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adventours.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
