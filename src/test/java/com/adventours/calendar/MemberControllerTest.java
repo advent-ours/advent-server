2e2fc1aef0bcab5c5ea148e7b5c3db246885e0c4
JinuCheon <woojin8787@gmail.com>
2023-09-22 13:39:57 +0900

    2. 카카오 oauth 로그인 테스트 실패

--- src/test/java/com/adventours/calendar/MemberControllerTest.java
+++ src/test/java/com/adventours/calendar/MemberControllerTest.java
@@ -3,11 +3,32 @@ package com.adventours.calendar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MemberControllerTest {
+
+    private MemberService memberService;
 
     @Test
     @DisplayName("카카오 로그인 성공")
     void login_kakao() {
-        userService.login(provider, request);
+        final OAuthProvider provider = OAuthProvider.KAKAO;
+        final String token = "loginToken";
+        final LoginRequest request = new LoginRequest(token);
+        boolean isNewMember = memberService.login(provider, request);
+        assertThat(isNewMember).isTrue();
+    }
+
+    private class MemberService {
+        public boolean login(final OAuthProvider provider, final LoginRequest request) {
+            throw new UnsupportedOperationException("MemberService#login not implemented yet.");
+        }
+    }
+
+    private enum OAuthProvider {
+        KAKAO
+    }
+
+    private record LoginRequest(String token) {
     }
 }
