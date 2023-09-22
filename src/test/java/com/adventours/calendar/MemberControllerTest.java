ac5fad6f960b876dd146bbeb6ac0cc7d939798aa
JinuCheon <woojin8787@gmail.com>
2023-09-22 14:00:26 +0900

    3. oauth 로그인 로직 구현

--- src/test/java/com/adventours/calendar/MemberControllerTest.java
+++ src/test/java/com/adventours/calendar/MemberControllerTest.java
@@ -3,6 +3,9 @@ package com.adventours.calendar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MemberControllerTest {
@@ -15,13 +18,34 @@ class MemberControllerTest {
         final OAuthProvider provider = OAuthProvider.KAKAO;
         final String token = "loginToken";
         final LoginRequest request = new LoginRequest(token);
-        boolean isNewMember = memberService.login(provider, request);
-        assertThat(isNewMember).isTrue();
+        final LoginResponse response = memberService.login(provider, request);
+        assertThat(response.isNewMember).isTrue();
     }
 
     private class MemberService {
-        public boolean login(final OAuthProvider provider, final LoginRequest request) {
-            throw new UnsupportedOperationException("MemberService#login not implemented yet.");
+        private final Map<OAuthProvider, OAuthRequestPort> oAuthRequestPortMap;
+        private final MemberRepository memberRepository;
+
+        public MemberService(final Map<OAuthProvider, OAuthRequestPort> oAuthRequestPortMap, final MemberRepository memberRepository) {
+            this.oAuthRequestPortMap = oAuthRequestPortMap;
+            this.memberRepository = memberRepository;
+        }
+
+        public LoginResponse login(final OAuthProvider provider, final LoginRequest request) {
+            OAuthRequestPort oAuthRequestPort = getProperProviderPort(provider);
+            OAuthUserInformation userInformation = oAuthRequestPort.requestUserInformation(request.token());
+            Optional<Member> optionalMember = memberRepository.findByProviderAndProviderId(provider, userInformation.providerId());
+            if (optionalMember.isPresent()) {
+                return new LoginResponse(optionalMember.get().getId(), false);
+            } else {
+                final Member member = memberRepository.save(new Member());
+                return new LoginResponse(member.getId(), true);
+            }
+
+        }
+
+        private OAuthRequestPort getProperProviderPort(final OAuthProvider provider) {
+            return oAuthRequestPortMap.get(provider);
         }
     }
 
@@ -30,5 +54,35 @@ class MemberControllerTest {
     }
 
     private record LoginRequest(String token) {
+    }
+
+    private interface OAuthRequestPort {
+        OAuthUserInformation requestUserInformation(String token);
+    }
+
+    public class KakaoOAuthRequestAdapter implements OAuthRequestPort {
+        @Override
+        public OAuthUserInformation requestUserInformation(final String token) {
+            throw new UnsupportedOperationException("KakaoOAuthRequestAdapter#requestUserInformation not implemented yet.")
+        }
+    }
+
+    private interface MemberRepository {
+        Optional<Member> findByProviderAndProviderId(OAuthProvider provider, String s);
+
+        Member save(Member member);
+    }
+
+    private record OAuthUserInformation(String providerId, String initialProfileImage) {
+    }
+
+    private class Member {
+        private Long id;
+        public Long getId() {
+            return id;
+        }
+    }
+
+    private record LoginResponse(long memberId, boolean isNewMember) {
     }
 }
