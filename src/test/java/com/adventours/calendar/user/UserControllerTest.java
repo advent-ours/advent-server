8930c42970a61168423dfe3b2db457ca32133d75
JinuCheon <woojin8787@gmail.com>
2023-09-22 15:55:40 +0900

    11. 기존 통합 테스트를 Rest Assured 컨트롤러 테스트로 전환

--- src/test/java/com/adventours/calendar/user/UserControllerTest.java
+++ src/test/java/com/adventours/calendar/user/UserControllerTest.java
@@ -1,16 +1,18 @@
 package com.adventours.calendar.user;
 
+import com.adventours.calendar.common.ApiTest;
 import com.adventours.calendar.user.client.KakaoOAuthFeignClient;
 import com.adventours.calendar.user.domain.OAuthProvider;
+import com.adventours.calendar.user.persistence.UserRepository;
 import com.adventours.calendar.user.service.KakaoUserInformation;
 import com.adventours.calendar.user.service.LoginRequest;
-import com.adventours.calendar.user.service.LoginResponse;
-import com.adventours.calendar.user.service.UserService;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,18 +20,17 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ActiveProfiles("test")
-@SpringBootTest
-class UserControllerTest {
-
-    @Autowired
-    UserService userService;
+class UserControllerTest extends ApiTest {
 
     @MockBean
     KakaoOAuthFeignClient kakaoInformationFeignClient;
+    @Autowired
+    UserRepository userRepository;
 
     @Test
     @DisplayName("카카오 로그인 성공")
     void login_kakao() {
+        //given
         final OAuthProvider provider = OAuthProvider.KAKAO;
         final String token = "loginToken";
         final LoginRequest request = new LoginRequest(token);
@@ -42,8 +43,17 @@ class UserControllerTest {
                 "kakaoAccount"
         ));
 
-        final LoginResponse response = userService.login(provider, request);
-        assertThat(response.isNewUser()).isTrue();
+        //when
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/user/login/oauth/{provider}", provider.name())
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value());
+
+        //then
+        assertThat(userRepository.findByProviderAndProviderId(provider, "providerId")).isPresent();
     }
 
 }
