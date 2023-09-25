66cc27ffa59e7027300a6ed69b91b128d0c30faa
JinuCheon <woojin8787@gmail.com>
2023-09-25 09:41:59 +0900

    6. 토큰 인증 테스트 추가

--- src/main/java/com/adventours/calendar/HelloController.java
+++ src/main/java/com/adventours/calendar/HelloController.java
@@ -1,13 +1,17 @@
 package com.adventours.calendar;
 
+import com.adventours.calendar.auth.Auth;
+import com.adventours.calendar.auth.UserContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Auth
     @GetMapping("/")
     public String hello() {
-        return "진우 지호 찬희 고운 화이팅";
+        final Long test = UserContext.CONTEXT.get();
+        return "진우 지호 찬희 고운 화이팅 " + test;
     }
 }
