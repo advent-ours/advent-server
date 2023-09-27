426c4269cd803faea43681f454446024508110b6
JinuCheon <woojin8787@gmail.com>
2023-09-28 08:52:51 +0900

    2. 캘린더 id를 uuid 로 지정

--- src/main/java/com/adventours/calendar/calendar/domain/Calendar.java
+++ src/main/java/com/adventours/calendar/calendar/domain/Calendar.java
@@ -6,21 +6,29 @@ import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
 
 @Getter
 @Entity
 @Table(name = "CALENDAR")
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Calendar {
 
     //TODO: UUID pk 적용
     @Id
-    @Column(name = "CALENDAR_ID")
-    private String id;
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name="uuid2", strategy = "uuid2")
+    @Column(name = "CALENDAR_ID", columnDefinition = "BINARY(16)")
+    private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
@@ -28,5 +36,10 @@ public class Calendar {
 
     @Column(nullable = false)
     private String title;
+
+    public Calendar(final User user, final String title) {
+        this.user = user;
+        this.title = title;
+    }
 
 }
