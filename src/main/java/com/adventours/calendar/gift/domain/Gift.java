ced21a2bbc572da302902551e4678ef57f585bdd
JinuCheon <woojin8787@gmail.com>
2023-09-27 15:55:43 +0900

    3. 선물 스키마 설정

---
+++ src/main/java/com/adventours/calendar/gift/domain/Gift.java
@@ -0,0 +1,38 @@
+package com.adventours.calendar.gift.domain;
+
+import com.adventours.calendar.calendar.domain.Calendar;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "GIFT")
+public class Gift {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "calendarId")
+    private Calendar calendar;
+
+    @Column(nullable = false)
+    private int day;
+
+    //TODO: Enum으로 제공
+    @Column(nullable = false)
+    private String contentType;
+
+    private String contentUrl;
+
+    private String body;
+
+    // 생성자, getter 및 setter 메서드
+}
