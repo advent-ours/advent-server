094297dbfbfaf1a31d92e47dbc66eca54dfc7351
JinuCheon <woojin8787@gmail.com>
2023-09-27 15:58:51 +0900

    4. 댓글 스키마 설정

--- src/main/java/com/adventours/calendar/gift/domain/Comment.java
+++ src/main/java/com/adventours/calendar/gift/domain/Comment.java
@@ -1,8 +1,10 @@
 package com.adventours.calendar.gift.domain;
 
-import com.adventours.calendar.calendar.domain.Calendar;
+import com.adventours.calendar.user.domain.User;
 import jakarta.persistence.Column;
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,26 +15,20 @@ import lombok.Getter;
 
 @Getter
 @Entity
-@Table(name = "GIFT")
-public class Gift {
+@Table(name = "COMMENT")
+public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long commentId;
 
     @ManyToOne
-    @JoinColumn(name = "calendarId", nullable = false)
-    private Calendar calendar;
+    @JoinColumn(name = "gift_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Gift gift;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private User user;
 
     @Column(nullable = false)
-    private int day;
-
-    //TODO: Enum으로 제공
-    @Column(nullable = false)
-    private String contentType;
-
-    private String contentUrl;
-
     private String body;
-
-    // 생성자, getter 및 setter 메서드
 }
