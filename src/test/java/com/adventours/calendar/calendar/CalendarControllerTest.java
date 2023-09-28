dcee57d3fa711fcfe7ab8ae3831b8f41e0beda30
JinuCheon <woojin8787@gmail.com>
2023-09-28 09:02:39 +0900

    5. 컨트롤러 테스트로 전환

--- src/test/java/com/adventours/calendar/calendar/CalendarControllerTest.java
+++ src/test/java/com/adventours/calendar/calendar/CalendarControllerTest.java
@@ -1,27 +1,34 @@
 package com.adventours.calendar.calendar;
 
 import com.adventours.calendar.calendar.persistence.CalendarRepository;
-import com.adventours.calendar.calendar.service.CalendarService;
 import com.adventours.calendar.calendar.service.CreateCalendarRequest;
 import com.adventours.calendar.common.ApiTest;
 import com.adventours.calendar.gift.persistence.GiftRepository;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CalendarControllerTest extends ApiTest {
 
-    @Autowired CalendarService calendarService;
     @Autowired CalendarRepository calendarRepository;
     @Autowired GiftRepository giftRepository;
 
     @Test
     void createCalendar() {
-        final Long userId = 1L;
         final CreateCalendarRequest request = new CreateCalendarRequest("제목");
-        calendarService.createCalendar(userId, request);
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .when()
+                .header("Authorization", accessToken)
+                .post("/calendar")
+                .then().log().all()
+                .statusCode(HttpStatus.CREATED.value());
 
         Assertions.assertAll(
             () -> assertThat(calendarRepository.count()).isEqualTo(1),
