9efdd95f52822804a30c8b389135fd5269eccec2
JinuCheon <woojin8787@gmail.com>
2023-09-30 09:16:51 +0900

    1. 캘린더 수정 테스트 작성

--- src/test/java/com/adventours/calendar/calendar/CalendarControllerTest.java
+++ src/test/java/com/adventours/calendar/calendar/CalendarControllerTest.java
@@ -3,12 +3,14 @@ package com.adventours.calendar.calendar;
 import com.adventours.calendar.calendar.persistence.CalendarRepository;
 import com.adventours.calendar.common.ApiTest;
 import com.adventours.calendar.common.Scenario;
+import com.adventours.calendar.gift.domain.Gift;
 import com.adventours.calendar.gift.persistence.GiftRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static com.adventours.calendar.gift.domain.GiftType.INIT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CalendarControllerTest extends ApiTest {
@@ -18,7 +20,7 @@ class CalendarControllerTest extends ApiTest {
 
     @Test
     @DisplayName("캘린더 생성 성공")
-    void request() {
+    void createCalendar() {
         Scenario.createCalendar().request();
         Assertions.assertAll(
             () -> assertThat(calendarRepository.count()).isEqualTo(1),
@@ -26,4 +28,18 @@ class CalendarControllerTest extends ApiTest {
         );
     }
 
+    @Test
+    @DisplayName("캘린더 생성/수정 성공")
+    void updateCalendar() {
+        final long userId = 1L;
+        final UpdateCalendarRequest request;
+
+        giftService.updateGift(userId, request);
+
+        final Gift gift = giftRepository.findById(1L).get();
+        Assertions.assertAll(
+                () -> assertThat(gift.getBody()).isEqualTo(""),
+                () -> assertThat(gift.getGiftType()).isEqualTo(INIT)
+        );
+    }
 }
