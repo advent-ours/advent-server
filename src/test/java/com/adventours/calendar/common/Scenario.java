b7e985f6cf7d0cb8ece728007cee17bd909ea1c3
JinuCheon <woojin8787@gmail.com>
2023-09-30 10:03:20 +0900

    7. 시나리오 테스트로 전환

--- src/test/java/com/adventours/calendar/common/Scenario.java
+++ src/test/java/com/adventours/calendar/common/Scenario.java
@@ -1,9 +1,14 @@
 package com.adventours.calendar.common;
 
 import com.adventours.calendar.api.CreateCalendarApi;
+import com.adventours.calendar.gift.api.UpdateGiftApi;
 
 public class Scenario {
     public static CreateCalendarApi createCalendar() {
         return new CreateCalendarApi();
+    }
+
+    public UpdateGiftApi updateGift() {
+        return new UpdateGiftApi();
     }
 }
--- src/test/java/com/adventours/calendar/gift/GiftControllerTest.java
+++ src/test/java/com/adventours/calendar/gift/GiftControllerTest.java
@@ -7,8 +7,6 @@ import com.adventours.calendar.gift.domain.Gift;
 import com.adventours.calendar.gift.domain.GiftType;
 import com.adventours.calendar.gift.persistence.GiftRepository;
 import com.adventours.calendar.gift.service.GiftService;
-import com.adventours.calendar.gift.service.UpdateGiftRequest;
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,30 +24,14 @@ class GiftControllerTest extends ApiTest {
     @Test
     @DisplayName("선물 생성/수정 성공")
     void updateGift() {
-        Scenario.createCalendar().request();
-        final long calendarId = 1L;
-        final long giftId = 1L;
-        final UpdateGiftRequest request = new UpdateGiftRequest(
-                GiftType.TEXT,
-                "제목",
-                "내용",
-                null
-        );
-
-        RestAssured.given().log().all()
-                .header("Authorization", accessToken)
-                .contentType("application/json")
-                .body(request)
-                .when()
-                .post("/calendar/{calendarId}/gifts/{giftId}", calendarId, giftId)
-                .then()
-                .statusCode(200);
+        Scenario.createCalendar().request()
+                .updateGift().request();
 
         final Gift gift = giftRepository.findById(1L).get();
         Assertions.assertAll(
-                () -> org.assertj.core.api.Assertions.assertThat(gift.getTitle()).isEqualTo(request.title()),
-                () -> org.assertj.core.api.Assertions.assertThat(gift.getTextBody()).isEqualTo(request.textBody()),
-                () -> org.assertj.core.api.Assertions.assertThat(gift.getGiftType()).isEqualTo(request.giftType())
+                () -> org.assertj.core.api.Assertions.assertThat(gift.getTitle()).isEqualTo("제목"),
+                () -> org.assertj.core.api.Assertions.assertThat(gift.getTextBody()).isEqualTo("내용"),
+                () -> org.assertj.core.api.Assertions.assertThat(gift.getGiftType()).isEqualTo(GiftType.TEXT)
         );
     }
 }
