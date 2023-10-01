329a0da3d59d38e44d17fceae6330370e27667dd
JinuCheon <woojin8787@gmail.com>
2023-10-01 22:55:58 +0900

    feat: health check 용 api 구현

--- src/main/java/com/adventours/calendar/HelloController.java
+++ src/main/java/com/adventours/calendar/HelloController.java
@@ -1,17 +1,13 @@
 package com.adventours.calendar;
 
-import com.adventours.calendar.auth.Auth;
-import com.adventours.calendar.auth.UserContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @Auth
     @GetMapping("/")
     public String hello() {
-        final Long test = UserContext.CONTEXT.get();
-        return "진우 지호 찬희 고운 화이팅 " + test;
+        return "진우 지호 찬희 고운 화이팅 ";
     }
 }
