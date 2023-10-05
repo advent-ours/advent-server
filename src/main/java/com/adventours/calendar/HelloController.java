a5bb28b4c1a087f817e43a85e5e089e750f95e31
JinuCheon <woojin8787@gmail.com>
2023-10-05 23:48:45 +0900

    1. api 예외 처리 핸들러 테스트를 위한 컨트롤러 생성

--- src/main/java/com/adventours/calendar/HelloController.java
+++ src/main/java/com/adventours/calendar/HelloController.java
@@ -1,6 +1,12 @@
 package com.adventours.calendar;
 
+import com.adventours.calendar.global.CommonResponse;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +15,17 @@ public class HelloController {
     @GetMapping("/")
     public String hello() {
         return "진우 지호 찬희 고운 화이팅 ";
+    }
+
+    @PostMapping("/exception/{path}")
+    public ResponseEntity<CommonResponse<Void>> testException(
+            @PathVariable(required = true) final String path,
+            @RequestBody final ExceptionTestBody body) {
+        return ResponseEntity.ok(new CommonResponse<>());
+    }
+
+    public record ExceptionTestBody(
+            @NotNull
+            int integerData) {
     }
 }
---
+++ src/test/java/com/adventours/calendar/exception/ExceptionTest.java
@@ -0,0 +1,11 @@
+package com.adventours.calendar.exception;
+
+import com.adventours.calendar.common.ApiTest;
+import org.junit.jupiter.api.Test;
+
+public class ExceptionTest extends ApiTest {
+
+    @Test
+    void name() {
+    }
+}
