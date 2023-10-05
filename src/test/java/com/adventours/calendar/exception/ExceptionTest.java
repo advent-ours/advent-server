0932518bae7c9048c3ec8bc7fb09881b789fe11d
JinuCheon <woojin8787@gmail.com>
2023-10-05 23:51:36 +0900

    2. 정상 테스트 성공

--- src/test/java/com/adventours/calendar/exception/ExceptionTest.java
+++ src/test/java/com/adventours/calendar/exception/ExceptionTest.java
@@ -1,11 +1,22 @@
 package com.adventours.calendar.exception;
 
+import com.adventours.calendar.HelloController;
 import com.adventours.calendar.common.ApiTest;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
-public class ExceptionTest extends ApiTest {
+class ExceptionTest extends ApiTest {
 
     @Test
-    void name() {
+    void ok() {
+        HelloController.ExceptionTestBody request = new HelloController.ExceptionTestBody(1);
+
+        RestAssured.given().log().all()
+                .when()
+                .body(request)
+                .contentType("application/json")
+                .post("/exception/1")
+                .then().log().all()
+                .statusCode(200);
     }
 }
