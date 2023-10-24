59afe5e96e20f763a7bcad2a10557617d1ca641d
JinuCheon <woojin8787@gmail.com>
2023-10-24 17:19:43 +0900

    5. 캘린더 목록 조회에 템플릿 필드 반영

--- src/main/java/com/adventours/calendar/calendar/service/CalendarListResponse.java
+++ src/main/java/com/adventours/calendar/calendar/service/CalendarListResponse.java
@@ -1,6 +1,7 @@
 package com.adventours.calendar.calendar.service;
 
 import com.adventours.calendar.calendar.domain.Calendar;
+import com.adventours.calendar.calendar.domain.CalendarTemplate;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,6 +12,7 @@ public record CalendarListResponse(
         Long userId,
         String profileImgUrl,
         String title,
+        CalendarTemplate template,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
 ) {
@@ -22,6 +24,7 @@ public record CalendarListResponse(
                         calendar.getUser().getId(),
                         calendar.getUser().getProfileImgUrl(),
                         calendar.getTitle(),
+                        calendar.getTemplate(),
                         calendar.getCreatedAt(),
                         calendar.getUpdatedAt()
                 )).toList();
