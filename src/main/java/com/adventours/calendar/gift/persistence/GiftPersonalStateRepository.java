4be385de6197fc1ef622e8dee6e6d3399c8e3f75
JinuCheon <woojin8787@gmail.com>
2023-10-29 11:19:50 +0900

    1. 읽지 않은 선물을 조회하는 native 쿼리 작성

--- src/main/java/com/adventours/calendar/gift/persistence/GiftPersonalStateRepository.java
+++ src/main/java/com/adventours/calendar/gift/persistence/GiftPersonalStateRepository.java
@@ -3,6 +3,17 @@ package com.adventours.calendar.gift.persistence;
 import com.adventours.calendar.gift.domain.GiftPersonalState;
 import com.adventours.calendar.gift.domain.GiftPersonalStatePk;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GiftPersonalStateRepository extends JpaRepository<GiftPersonalState, GiftPersonalStatePk> {
+    @Query(value =
+            "select count(*) " +
+            "from gift_personal_state ps " +
+            "join gift g " +
+            "on ps.gift_id = g.gift_id " +
+            "where g.calendar_id = :calendarId " +
+            "and ps.user_id = :userId " +
+            "and ps.is_opened = false",
+            nativeQuery = true)
+    Long countNotReadGift(String calendarId, Long userId);
 }
