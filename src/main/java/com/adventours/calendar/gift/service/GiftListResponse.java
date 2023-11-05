bd0be29e6f34f925e34fe016d838192f8ab4c4ea
JinuCheon <woojin8787@gmail.com>
2023-11-05 19:06:27 +0900

    3. 선물 리스트 조회에서 디테일한 정보 삭제

--- src/main/java/com/adventours/calendar/gift/service/GiftListResponse.java
+++ src/main/java/com/adventours/calendar/gift/service/GiftListResponse.java
@@ -1,20 +1,12 @@
 package com.adventours.calendar.gift.service;
 
-import com.adventours.calendar.gift.domain.GiftReact;
-import com.adventours.calendar.gift.domain.GiftType;
-
 import java.time.LocalDateTime;
 
 public record GiftListResponse(
         Long giftId,
         LocalDateTime openAt,
-        GiftType giftType,
-        String title,
-        String textBody,
-        String contentUrl,
         LocalDateTime createdAt,
         LocalDateTime updatedAt,
-        boolean isOpened,
-        GiftReact react
+        boolean isOpened
 ) {
 }
