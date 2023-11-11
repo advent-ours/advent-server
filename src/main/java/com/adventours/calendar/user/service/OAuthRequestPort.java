676458e5313920cb54580db080ca73141a516157
JinuCheon <woojin8787@gmail.com>
2023-11-12 00:52:02 +0900

    1. 기존 카카오 로그인 클래스명 변경

--- src/main/java/com/adventours/calendar/user/service/OAuthRequestPort.java
+++ src/main/java/com/adventours/calendar/user/service/OAuthRequestPort.java
@@ -3,7 +3,7 @@ package com.adventours.calendar.user.service;
 import com.adventours.calendar.user.domain.OAuthProvider;
 
 interface OAuthRequestPort {
-    OAuthProviderInformation requestUserInformation(String token);
+    OAuthUserInformation requestUserInformation(String token);
 
     OAuthProvider getOAuthProvider();
 }
