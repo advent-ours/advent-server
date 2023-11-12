30f737ad07cbe19189c5dcbe1772509dc963cc30
JinuCheon <woojin8787@gmail.com>
2023-11-12 21:51:52 +0900

    6. CommonController -> DailySentenceController 이름 변경

--- src/main/java/com/adventours/calendar/dailysentence/DailySentenceController.java
+++ src/main/java/com/adventours/calendar/dailysentence/DailySentenceController.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
-public class CommonController {
+public class DailySentenceController {
 
     private final DailySentenceRepository dailySentenceRepository;
     private final Clock clock;
