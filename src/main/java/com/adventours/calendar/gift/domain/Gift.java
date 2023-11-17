80d6f5f6ee352b0a51830c2043040386a833a5fe
Lego(천진우) <woojin8787@gmail.com>
2023-11-17 12:52:22 +0900

    5. wildcard import 수정 이후 적용되지 않는 Import 처리

--- src/main/java/com/adventours/calendar/gift/domain/Gift.java
+++ src/main/java/com/adventours/calendar/gift/domain/Gift.java
@@ -5,6 +5,8 @@ import com.adventours.calendar.global.BaseTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
