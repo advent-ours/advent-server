749235ce63a7f12704e2e944e6739e3bfda8992f
Lego8787 <lego@stclab.com>
2023-12-03 16:03:32 +0900

    fix: 오늘의 문장 표기일을 올 해로 선택

--- src/main/java/com/adventours/calendar/dailysentence/DailySentenceController.java
+++ src/main/java/com/adventours/calendar/dailysentence/DailySentenceController.java
@@ -19,8 +19,8 @@ public class DailySentenceController {
     @RequestMapping("/dailysentence")
     public ResponseEntity<CommonResponse<String>> dailySentence() {
         LocalDate today = LocalDate.now(clock);
-        LocalDate startOfService = LocalDate.of(2024, 12, 1);
-        LocalDate endOfService = LocalDate.of(2024, 12, 25);
+        LocalDate startOfService = LocalDate.of(2023, 12, 1);
+        LocalDate endOfService = LocalDate.of(2023, 12, 25);
         // 2023년 12월 이전
         if (today.isBefore(startOfService)) {
             return ResponseEntity.ok(new CommonResponse<>("크리스마스가 얼마 남지 않았어요."));
